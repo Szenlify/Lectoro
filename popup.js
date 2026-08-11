e66d7cdae0b9533d79f663d56c0f0c200175d109
Seovileoo <szenlify@gmail.com>
2026-08-11 13:20:43 +0200

    dfg

--- popup.js
+++ popup.js
@@ -1318,7 +1318,7 @@ function popupSpeak(text, lang) {
                                 },
                                 body: JSON.stringify({
                                     text: cleanTextForPopupTTS(text),
-                                    model_id: "eleven_multilingual_v2",
+                                    model_id: "eleven_flash_v2_5",
                                     voice_settings: {
                                         stability: 0.5,
                                         similarity_boost: 0.75,
