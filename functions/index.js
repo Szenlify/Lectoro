45d8c259cbfe700f638368fbc6711928bbd27c52
Softileo <szenlify@gmail.com>
2026-08-23 21:05:08 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -501,7 +501,9 @@ exports.geminiProxy = onRequest(
                         body: JSON.stringify({
                             text,
                             model_id: "eleven_flash_v2_5",
-                            voice_settings: { stability: 0.5, similarity_boost: 0.75 },
+                            voice_settings: { stability: 0.7, similarity_boost: 0.75 },
+                            style: 0.2,
+                            use_speaker_boost: true,
                         }),
                     },
                 );
